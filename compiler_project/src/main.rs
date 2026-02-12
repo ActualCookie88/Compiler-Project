97f6ea8170dd0d7e624e12666c944575ad5d23e7
Kim Nguyen <knguy863@xe-10.cs.ucr.edu>
2026-02-12 02:10:40 +0000

    all .tt passed

--- compiler_project/src/main.rs
+++ compiler_project/src/main.rs
@@ -376,6 +376,17 @@ fn parse_function(tokens: &Vec<Token>, index: &mut usize) -> Result<(), String> 
     _ => { return Err(String::from("Missing the left parenthesis '('"));}
     }
 
+    if !matches!(tokens[*index], Token::RightParen) {
+        // first param
+        parse_declaration_statement_for_function(tokens, index)?;
+
+        // more params
+        while matches!(tokens[*index], Token::Comma) {
+            *index += 1; 
+            parse_declaration_statement_for_function(tokens, index)?;
+        }
+    }
+
     match tokens[*index] {
     Token::RightParen => { *index += 1; }
     _ => { return Err(String::from("Missing the right parenthesis ')'"));}
@@ -471,6 +482,21 @@ fn parse_declaration_statement(tokens: &Vec<Token>, index: &mut usize) -> Result
       _ => {return Err(String::from("Declaration statements must begin with 'int' keyword"));}
     }
 
+    // [expression]
+    if matches!(tokens[*index], Token::LeftBracket) {
+      *index += 1;
+
+      match tokens[*index] {
+          Token::Num(_) => { *index += 1; }
+          _ => { return Err(String::from("Expected number within")); }
+      }
+
+      match tokens[*index] {
+          Token::RightBracket => { *index += 1; }
+          _ => { return Err(String::from("Expected ']' after array size")); }
+      }
+
+    }
     // identifier
     match tokens[*index] {
       Token::Ident(_) => {*index += 1;}
@@ -517,6 +543,22 @@ fn parse_declaration_statement(tokens: &Vec<Token>, index: &mut usize) -> Result
       }
       _ => {return Err(String::from("Statement must end with a semicolon"));}
     }
+}
+
+fn parse_declaration_statement_for_function(tokens: &Vec<Token>, index: &mut usize) -> Result<(), String> {
+    // int
+    match tokens[*index] {
+        Token::Int => { *index += 1; }
+        _ => { return Err(String::from("Declaration statements must begin with 'int' keyword")); }
+    }
+
+    // identifier
+    match tokens[*index] {
+        Token::Ident(_) => { *index += 1; }
+        _ => { return Err(String::from("Declarations must have an identifier")); }
+    }
+
+    return Ok(());
 }
 
 fn parse_assignment_statement(tokens: &Vec<Token>, index: &mut usize) -> Result<(), String> {
@@ -700,6 +742,28 @@ fn parse_term(tokens: &Vec<Token>, index: &mut usize) -> Result<(), String> {
               _ => { return Err(String::from("Expected ']' after array size")); }
           }
         }
+        
+        if matches!(tokens[*index], Token::LeftParen) {
+          *index += 1;
+
+          while !matches!(tokens[*index], Token::RightParen) {
+            match parse_expression(tokens, index) {
+                Ok(()) => {}
+                Err(e) => { return Err(e); }
+            }
+            if matches!(tokens[*index], Token::Comma) {
+                *index += 1;
+            } else {
+                break;
+            }
+          }
+
+          match tokens[*index] {
+              Token::RightParen => { *index += 1; }
+              _ => { return Err(String::from("Expected ')' after function call")); }
+          }
+        }
+
         return Ok(());
     }
 
